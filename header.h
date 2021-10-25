8302f0b6f63f3afef45f153b2d8d0569036cea1f
git-me-coffee <92636553+git-me-coffee@users.noreply.github.com>
2021-10-25 12:57:01 +0200

    Create header.h

---
+++ header.h
@@ -0,0 +1,31 @@
+struct Node
+{
+	int key;
+	Node* prev;
+	Node* left;
+	Node* right;
+}
+
+struct Result
+{
+
+	Node* node;
+	bool success;
+	int comparisons;
+
+}
+
+class BinaryTree
+
+{
+    private:
+        Node* root;
+
+    public:
+        BinaryTree();
+        ~BinaryTree();
+        
+        Result insertKey(int key);
+        Result searchKey(int key);
+        Result removeKey(int key);
+};
